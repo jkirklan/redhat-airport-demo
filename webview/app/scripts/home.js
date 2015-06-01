640ee834e7ad30633518a3590536ca22bda23f55
Phong Huynh <phuynh@architech.ca>
2015-06-01 17:00:44 -0400

    Create javascript modules

---
+++ webview/app/scripts/home.js
@@ -0,0 +1,12 @@
+/* jshint unused:false, devel:true */
+var home = (function($, window, document) {
+  'use strict';
+
+  var home = {
+    init: function() {
+      console.log('test');
+    }
+  };
+
+  return home;
+}(jQuery, window, document));

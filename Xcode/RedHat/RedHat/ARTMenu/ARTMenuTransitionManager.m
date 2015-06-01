1af98fbfe32b50510b9ae3355f8d8e6e5180e985
Mike Post <mpost@architech.ca>
2015-06-01 16:25:33 -0400

    Implemented animateTransition UIViewControllerAnimatedTransitioning method

--- Xcode/RedHat/RedHat/ARTMenu/ARTMenuTransitionManager.m
+++ Xcode/RedHat/RedHat/ARTMenu/ARTMenuTransitionManager.m
@@ -76,4 +76,52 @@
 }
 
 
+- (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
+{
+    UIView *container = [transitionContext containerView];
+    UIView *dimmingView = [[container subviews] firstObject];
+    
+    UIView *fromView = [[transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey] view];
+    UIView *toView = [[transitionContext viewControllerForKey:UITransitionContextToViewControllerKey] view];
+    
+    CGAffineTransform offScreenRight = CGAffineTransformMakeTranslation((container.bounds.size.width * 0.8), 0);
+    CGAffineTransform offScreenLeft = CGAffineTransformMakeTranslation(-container.bounds.size.width, 0);
+    
+    if (self.presenting == YES) {
+        toView.transform = offScreenLeft;
+    }
+
+    [container addSubview:fromView];
+#warning - BUG: dimming view dissapears during a custom animation
+    [container addSubview:dimmingView];
+    [container addSubview:toView];
+    
+    //Perform animation...
+    [UIView animateWithDuration:self.duration
+                          delay:0
+         usingSpringWithDamping:1.0
+          initialSpringVelocity:0.5
+                        options:UIViewAnimationOptionTransitionNone
+                     animations:^{
+                         if ([self isPresenting]) {
+                             fromView.transform = offScreenRight;
+                             toView.transform = CGAffineTransformIdentity;
+                         }
+                         else {
+                             fromView.transform = offScreenLeft;
+                             toView.transform = CGAffineTransformIdentity;
+                         }
+                     }
+                     completion:^(BOOL finished) {
+                         //Tell our transitionContext object that we've finished animating...
+                         [transitionContext completeTransition:YES];
+                         
+                         #warning - BUG: we have to manually add our 'to view' back http://openradar.appspot.com/radar?id=5320103646199808
+                         [[[UIApplication sharedApplication] keyWindow] addSubview:toView];
+                     }];
+}
+
+
+
 @end
+

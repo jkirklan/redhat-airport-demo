f799b54ec1423c7c26c213074ce9208879b4e910
Phong Huynh <phuynh@architech.ca>
2015-06-03 17:12:18 -0400

    Functionality for coupon details page

--- webview/app/scripts/coupon-details.js
+++ webview/app/scripts/coupon-details.js
@@ -6,15 +6,28 @@ var couponDetails = (function($, window, document) {
     init: function() {
       this.couponData = JSON.parse(localStorage.getItem('coupons')) || '';
 
+      this.getCouponDetails();
+      this.parseFlightDate();
       this.compileHandlebarsTemplate();
     },
     getCouponDetails: function() {
-      // Use query string 
+      var couponId = this.getQueryString('id');
+
+      this.couponDetails = this.couponData[0];
+    },
+    getQueryString: function(name) {
+      name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+      var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
+          results = regex.exec(location.search);
+      return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    },
+    parseFlightDate: function() {
+      this.couponDetails.flight.flightDate = moment(this.couponDetails.flight.departure).format('ddd, MMM DD YYYY');
     },
     compileHandlebarsTemplate: function() {
       var source = $('#coupon-details-tmpl').html(),
           template = Handlebars.compile(source),
-          html = template(this.couponData);
+          html = template(this.couponDetails);
           
       $('.coupon-details-container').append(html);
     }

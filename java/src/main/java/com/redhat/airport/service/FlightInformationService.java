8d99271dba9fd19806f1bb2648a8a50314c31235
Fortunato <fguanlao@architech.ca>
2015-06-03 19:34:01 -0400

    Update flight service

--- java/src/main/java/com/redhat/airport/service/FlightInformationService.java
+++ java/src/main/java/com/redhat/airport/service/FlightInformationService.java
@@ -2,30 +2,29 @@ package com.redhat.airport.service;
 
 import com.redhat.airport.model.Flight;
 
-public class FlightInformationService
-{
-  public Flight getFlightInfo(int flightNo)
-  {
-    Flight flight = parseFlightData();
-    
-    return flight;
-  }
-  
-  public Flight parseFlightData()
-  {
-    Flight flight = new Flight();
-    flight.setFlightNo(70);
-    flight.setAirlineCode("AC");
-    flight.setDeparture("2015-06-02T12:00");
-    flight.setArrival("2015-06-02T13:45");
-    flight.setBoarding("2015-06-02T11:40");
-    flight.setStartingAirport("BOS");
-    flight.setDestinationAirport("YYZ");
-    flight.setStartingCity("Boston");
-    flight.setDestinationCity("Toronto");
-    flight.setFlightStatus("On Time");
-    flight.setStartingGate(1);
-    flight.setDestinationGate(1);
-    return flight;
-  }
+public class FlightInformationService {
+	public Flight getFlightInfo(int flightNo) {
+		Flight flight = retrieveFlightInformation(true);
+
+		return flight;
+	}
+
+	public Flight retrieveFlightInformation(boolean demoMode) {
+		Flight flight = new Flight();
+		flight.setFlightNo(70);
+		flight.setAirlineCode("AC");
+		flight.setDeparture("2015-06-02T12:00");
+		flight.setArrival("2015-06-02T13:45");
+		flight.setBoarding("2015-06-02T11:40");
+		flight.setStartingAirport("BOS");
+		flight.setDestinationAirport("YYZ");
+		flight.setStartingCity("Boston");
+		flight.setDestinationCity("Toronto");
+		flight.setFlightStatus("On Time");
+		flight.setStartingGate(1);
+		flight.setDestinationGate(1);
+
+		flight.setCoupon(null);
+		return flight;
+	}
 }
--- java/target/redhatairportdemo/WEB-INF/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO flight_status(status_type) VALUES('Changed');
-INSERT INTO flight_status(status_type) VALUES('Delayed');
-INSERT INTO flight_status(status_type) VALUES('Cancelled');
-
-INSERT INTO coupon(company,company_code,path,offer,description,delay_severity, status_id) VALUES('Starbucks','STBK','path','Get 1 Free Starbucks Coffee', 'Redeem this coupon at any Starbucks Coffee location at Toronto Pearson International.', 60, 2);

fea1d610fc1330d8b0123f687a0d524319b81ba7
Phong Huynh <phuynh@architech.ca>
2015-06-04 16:06:35 -0400

    Styling for delayed get coupon button

--- webview/app/scripts/home.js
+++ webview/app/scripts/home.js
@@ -36,62 +36,62 @@ var home = (function($, window, document) {
 
       url = demoMode ? '/rest/flightStatus/70?demoMode=true' : '/rest/flightStatus/70';
 
-      request = $.ajax({
-        url: url,
-        method: 'GET'
-      });
+      // request = $.ajax({
+      //   url: url,
+      //   method: 'GET'
+      // });
       
-      request.done(function(data) {
-        mockData = data;
-      });
+      // request.done(function(data) {
+      //   mockData = data;
+      // });
       
-      request.fail(function(jqXHR, textStatus) {
-        console.log('Request failed: ' + textStatus);
-      });
+      // request.fail(function(jqXHR, textStatus) {
+      //   console.log('Request failed: ' + textStatus);
+      // });
       
-      // if (demoMode === true) {
-      //   flightData = {
-      //     'flightNo': 70,
-      //     'airlineCode': 'AC',
-      //     'departure': '2015-06-02T12:00',
-      //     'arrival': '2015-06-02T13:45',
-      //     'boarding': '2015-06-02T11:40',
-      //     'startingAirport': 'BOS',
-      //     'destinationAirport': 'YYZ',
-      //     'startingCity': 'Boston',
-      //     'destinationCity': 'Toronto',
-      //     'flightStatus': 'Delayed',
-      //     'startingGate': 1,
-      //     'destinationGate': 1,
-      //     'coupon': {
-      //         'id': 0,
-      //         'company': 'Starbucks',
-      //         'companyCode': 'STBK',
-      //         'path': 'images/starbucks-coupon.png',
-      //         'offer': 'Get 1 Free Starbucks Coffee',
-      //         'description': 'Redeem this coupon at any Starbucks Coffee location at Boston Logan International.',
-      //         'delaySeverity': 60,
-      //         'statusId': 2
-      //     }
-      //   };
-      // }
-      // else {
-      //   flightData = {
-      //     'flightNo': 70,
-      //     'airlineCode': 'AC',
-      //     'departure': '2015-06-02T12:00',
-      //     'arrival': '2015-06-02T13:45',
-      //     'boarding': '2015-06-02T11:40',
-      //     'startingAirport': 'BOS',
-      //     'destinationAirport': 'YYZ',
-      //     'startingCity': 'Boston',
-      //     'destinationCity': 'Toronto',
-      //     'flightStatus': 'On Time',
-      //     'startingGate': 1,
-      //     'destinationGate': 1,
-      //     'coupon': null
-      //   };
-      // }
+      if (demoMode === true) {
+        flightData = {
+          'flightNo': 70,
+          'airlineCode': 'AC',
+          'departure': '2015-06-02T12:00',
+          'arrival': '2015-06-02T13:45',
+          'boarding': '2015-06-02T11:40',
+          'startingAirport': 'BOS',
+          'destinationAirport': 'YYZ',
+          'startingCity': 'Boston',
+          'destinationCity': 'Toronto',
+          'flightStatus': 'Delayed',
+          'startingGate': 1,
+          'destinationGate': 1,
+          'coupon': {
+              'id': 0,
+              'company': 'Starbucks',
+              'companyCode': 'STBK',
+              'path': 'images/starbucks-coupon.png',
+              'offer': 'Get 1 Free Starbucks Coffee',
+              'description': 'Redeem this coupon at any Starbucks Coffee location at Boston Logan International.',
+              'delaySeverity': 60,
+              'statusId': 2
+          }
+        };
+      }
+      else {
+        flightData = {
+          'flightNo': 70,
+          'airlineCode': 'AC',
+          'departure': '2015-06-02T12:00',
+          'arrival': '2015-06-02T13:45',
+          'boarding': '2015-06-02T11:40',
+          'startingAirport': 'BOS',
+          'destinationAirport': 'YYZ',
+          'startingCity': 'Boston',
+          'destinationCity': 'Toronto',
+          'flightStatus': 'On Time',
+          'startingGate': 1,
+          'destinationGate': 1,
+          'coupon': null
+        };
+      }
 
       localStorage.setItem('flightData', JSON.stringify(flightData));
       
@@ -118,6 +118,8 @@ var home = (function($, window, document) {
       else if (this.flightData.flightStatus === 'Delayed') {
         $departureTime.addClass('failure-color');
         $flightStatus.addClass('failure-bg');
+        $flightStatus.addClass('failure-bg-get-coupon');
+        $flightStatus.parent().append('<div class="get-coupon">Get Coupon</div>');
       }
       else if (this.flightData.flightStatus === 'Changed') {
         $departureTime.addClass('failure-color');

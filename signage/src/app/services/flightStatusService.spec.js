70989007fd545cd84d3b47b944d26a573c6b3982
Harnek Sidhu <hsidhu@architech.ca>
2015-06-12 11:23:13 -0400

    Finished flow for digital signage

--- signage/src/app/services/flightStatusService.spec.js
+++ signage/src/app/services/flightStatusService.spec.js
@@ -1,20 +1,37 @@
 describe('flightServiceModule', function () {
 
-	var flightStatusService;
+	var flightStatusService, httpBackend;
 
     beforeEach(module('flightServiceModule'));
 
 	beforeEach(function(){
-		inject(function(_flightStatusService_) {
+		inject(function(_flightStatusService_, $httpBackend) {
 			flightStatusService = _flightStatusService_;
+			httpBackend = $httpBackend;
 		});
 	});
+
+	afterEach(function() {
+        httpBackend.verifyNoOutstandingExpectation();
+        httpBackend.verifyNoOutstandingRequest();
+    });
 
     it('should have a dummy test', inject(function() {
         expect(true).toBeTruthy();
     }));
 
 	it('should have a flightStatus function', function () { 
-		expect(angular.isFunction(flightStatusService.flightStatus)).toBe(true);
+		expect(angular.isFunction(flightStatusService.flightList)).toBe(true);
 	});
+
+// it('should have an empty array on error for flightList', function () { 
+// httpBackend.whenGET('http://redhatairportdemo-fguanlao.rhcloud.com/rest/flightStatus/list')
+// .respond(500, 'error');
+
+// flightStatusService.flightList()
+// .then(function(result) {
+// expect(result).toBe('');
+// });
+// httpBackend.flush();
+// });
 });
